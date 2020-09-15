67de34089afd9ce1a3e8bd2eebbfcca607686025
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-15 14:44:55 +0200

    Refactored statistics, hall of fame, evaluation function to new module.

--- deap_specialist.py
+++ deap_specialist.py
@@ -4,7 +4,7 @@ sys.path.insert(0, 'evoman')
 import os
 import numpy as np
 import pandas as pd
-from scipy.spatial import distance
+
 import random
 from datetime import datetime
 import json
@@ -13,12 +13,10 @@ from deap import base, creator, tools, algorithms
 from simple_controller import player_controller
 from evoman.environment import Environment
 
+import evo_utils
+
 os.putenv("SDL_VIDEODRIVER", "fbcon")
 os.environ["SDL_VIDEODRIVER"] = 'dummy'
-
-"""
-Reference: https://deap.readthedocs.io/en/master/overview.htm
-"""
 
 # Set directory for saving logs and experiment states
 EXPERIMENT_DIRECTORY = 'experiments/tmp'
@@ -28,6 +26,15 @@ if not os.path.exists(EXPERIMENT_DIRECTORY):
 N_HIDDEN_NEURONS = 10  # how many neurons in the hidden layer of the NN
 # number of weights for multilayer with 10 hidden neurons (assuming 20 sensors)
 IND_SIZE = (21) * N_HIDDEN_NEURONS + (N_HIDDEN_NEURONS+1)*5
+
+# Define some NB parameters for our EA
+CXPB = 0.5  # Probability of mating two individuals
+MUTPB = 0.2  # Probability of mutating an individual
+NGEN = 5  # The number of generations
+POPSIZE = 10  # Number of individuals per generation (population size)
+HOFSIZE = 5  # Maximum size of hall of fame (best genomes)
+
+
 # Initialise the controller (neural network) for our AI player
 nn_controller = player_controller(N_HIDDEN_NEURONS)
 
@@ -51,111 +58,38 @@ toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 
-# We define our evaluation function
-def evaluate(individual: list) -> list:
-    """Custom evaluation function based on evoman specialist demo (NN)
-
-    Parameters
-    ----------
-    individual : list
-        The genotype of the individual
-
-    Returns
-    -------
-    list
-        The fitness score(s)
-    """
-
-    # initializes simulation in individual evolution mode, for single static enemy.
-    env = Environment(
-        experiment_name=EXPERIMENT_DIRECTORY,
-        multiplemode="no",
-        enemies=[2],                 # 1 to 8
-        playermode="ai",
-        enemymode="static",
-        player_controller=nn_controller,
-        level=2,
-        speed="fastest",
-        inputscoded="no",            # yes or no
-        randomini="no",              # yes or no
-        sound="off",                 # on or off
-        logs="off",                   # on or off
-        savelogs="yes",              # yes or no
-        clockprec="low",
-        timeexpire=3000,             # integer
-        overturetime=100,            # integer
-        solutions=None,              # any
-        fullscreen=False,            # True or False
-    )
-
-    # Run the simulation (score fitness)
-    fitness, player_life, enemy_life, sim_time = env.play(
-        pcont=np.array(individual))
-
-    return [fitness]
-
-
-# We then add the evaluation function to our toolbox
-toolbox.register("evaluate", evaluate)
+# We then add our custom evaluation function to our toolbox
+toolbox.register("evaluate", evo_utils.evaluate,
+                 player_controller=nn_controller,
+                 experiment_name=EXPERIMENT_DIRECTORY)
 
 # Initialise our population
-pop = toolbox.population(n=10)
-
-# Define some NB parameters for our EA
-CXPB = 0.5  # Probability of mating two individuals
-MUTPB = 0.2  # Probability of mutating an individual
-NGEN = 5  # The number of generations
+pop = toolbox.population(n=POPSIZE)
 
 # Create Hall of Fame (keeps N best individuals over all history)
-hall_of_fame = tools.HallOfFame(maxsize=10)
+hall_of_fame = tools.HallOfFame(maxsize=HOFSIZE)
 
-# Create a statistics object to log stats about `fitness` to our logbook
-fitness_stats = tools.Statistics(key=lambda ind: ind.fitness.values)
-# Add some keys and functions to the statistics object
-fitness_stats.register("min_fitness", np.min)
-fitness_stats.register("mean_fitness", np.mean)
-fitness_stats.register("std_fitness", np.std)
-fitness_stats.register("max_fitness", np.max)
+# Make a single statistics object to monitor fitness and genome stats
+stats = evo_utils.make_custom_statistics()
 
-
-def diversity_L1(pop):
-    """ Calculate mean Manhattan (L1) distance between every pair
-        of genomes in the population.
-    """
-    dists = [distance.cityblock(i, j) for i in pop for j in pop if i != j]
-    return np.mean(dists)
-
-
-# Create a statistics object to log stats about `genome` to our logbook
-genome_stats = tools.Statistics(key=lambda ind: ind)
-# Add some keys and functions to the statistics object
-genome_stats.register("diversity", diversity_L1)
-genome_stats.register("genome_size", lambda pop: np.sum([len(i) for i in pop]))
-
-# Make a single statistics object from both our stats objects
-stats = tools.MultiStatistics(fitness=fitness_stats, genome=genome_stats)
-
-
-print("Running Simple EA...")
+print(f"\nRunning EA for {NGEN} generations...\n")
 # We will use one of DEAP's provided evolutionary algorithms for now
 final_population, logbook = algorithms.eaSimple(
     pop, toolbox, CXPB, MUTPB, NGEN,
     halloffame=hall_of_fame, stats=stats, verbose=True)
 
-# Build dataframe of results
-df_fitness = pd.DataFrame(logbook.chapters['fitness'])
-df_genome = pd.DataFrame(logbook.chapters['genome'])
-df_stats = df_fitness.merge(df_genome, on=['gen', 'nevals'])
-df_stats.set_index('gen', inplace=True)
+# Get dataframe of stats
+df_stats = evo_utils.compile_stats(logbook)
+print('\nFinal results:\n')
 print(df_stats)
 
-# Save logbook data to timestamped CSV file
-now = datetime.now().strftime("%m-%d-%H_%M_%S")
-df_stats.to_csv(f"{EXPERIMENT_DIRECTORY}/{now}_logbook.csv")
+# Get dictionary of best_individuals -> {fitness: [genome], ...}
+best_individuals = evo_utils.compile_best_individuals(hall_of_fame)
+top_scores = sorted(list(best_individuals.keys()), reverse=True)
+print(f"\nTop scores: {top_scores}")
 
-# Save hall of fame (the best genotypes over all generations)
-best_individuals = {}
-for key, item in zip(hall_of_fame.keys, hall_of_fame.items):
-    best_individuals[key.values[0]] = item
+# Save the statistics to CSV and the best individuals to JSON
+now = datetime.now().strftime("%m-%d-%H_%M_%S")  # Timestamp
+df_stats.to_csv(f"{EXPERIMENT_DIRECTORY}/{now}_logbook.csv")
 with open(f"{EXPERIMENT_DIRECTORY}/{now}_best_individuals.json", 'w') as file:
     json.dump(best_individuals, file)
