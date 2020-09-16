4a9d299791d77ed060481be971b9e5526678d260
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-16 15:36:28 +0200

    Parallelised the inner loop of evolution experiment with joblib, but race conditions :(

--- experiments_evolve.py
+++ experiments_evolve.py
@@ -12,12 +12,31 @@ import pandas as pd
 import evo_utils
 from EA_demo import MyDemoEAInstance
 
+from joblib import Parallel, delayed
+import multiprocessing
+
 os.putenv("SDL_VIDEODRIVER", "fbcon")
 os.environ["SDL_VIDEODRIVER"] = 'dummy'
 
 SAVEPATH = 'results'
 ENEMIES = [1, 3, 5]
 REPEATS = 10
+JOBS = 1
+
+
+def evolve_island(ea_instance, enemy):
+    pop, stats, bests = ea_instance(enemies=[enemy]).evolve(verbose=False)
+
+    # Save the stats (fitness and genome)
+    stats['enemy'] = enemy
+    stats['ea_instance'] = str(ea_instance)
+
+    # Store the best genome
+    best_fitness = np.max(list(bests.keys()))
+    top_genome = bests[best_fitness]
+
+    return stats, best_fitness, top_genome
+
 
 # Make sure savepath exists
 if not os.path.exists(SAVEPATH):
@@ -28,29 +47,30 @@ best_performers = {}
 # Initialise list for saving statistic logs
 results = []
 
+
 for ea_instance in [MyDemoEAInstance, ]:
     # Instantiate nested dictionary for this instance
     best_performers[str(ea_instance)] = {e: [] for e in ENEMIES}
     for enemy in ENEMIES:
-        for repeat in range(REPEATS):
-            pop, stats, bests = ea_instance(enemies=[enemy]).evolve()
 
-            # Save the stats (fitness and genome)
-            stats['enemy'] = enemy
-            stats['ea_instance'] = str(ea_instance)
+        # Parallel execution of `evolve_island` function for N repeats
+        island_results = Parallel(n_jobs=JOBS)(
+            delayed(evolve_island)(ea_instance, enemy) for repeat in range(REPEATS))
+
+        for res in island_results:
+            stats, best_fitness, top_genome = res
+            best_performers[str(ea_instance)][enemy].append(top_genome)
             results.append(stats)
 
-            # Store the best genome
-            best_fitness = np.max(list(bests.keys()))
-            top_genome = bests[best_fitness]
-            best_performers[str(ea_instance)][enemy].append(top_genome)
 
 now = datetime.now().strftime("%m-%d-%H_%M_%S")  # Timestamp
 
 # Combine the overall statistics
 df_results = pd.concat(results)
 # Save ALL the results to CSV
-df_results.to_csv(f"{SAVEPATH}/{now}_online_results.csv")
+f_name = f"{SAVEPATH}/{now}_online_results.csv"
+df_results.to_csv(f_name)
+print(f"\nResults saved to {f_name}")
 
 # Save the best performers to JSON
 with open(f"{SAVEPATH}/{now}_best_genomes.json", 'w') as file:
