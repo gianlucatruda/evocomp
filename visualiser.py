aacae740a827bbfdb70a0b895ab5c846bbc17533
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-22 14:42:43 +0200

    Made code more modular by separating plotting, summarisation, tests.

--- visualiser.py
+++ visualiser.py
@@ -31,12 +31,24 @@ def format_online_results(df: pd.DataFrame):
     return _df
 
 
+def format_offline_results(df: pd.DataFrame):
+    """ Formats the raw offline results by calculating
+    statistics (mean) over all iterations.
+    """
+    # Make a copy of raw data
+    _df = df.copy()
+
+    # Calculate statistics over all iterations
+    _df = _df.groupby(['ea_instance', 'enemy', 'individual'], as_index=False).mean()
+    _df.reset_index(inplace=True)
+
+    return _df
+
+
 def specialist_lineplots(df: pd.DataFrame):
     """
     Compare your algorithms by enemy, making a line-plot across the generations, with the average/std (for the mean and the maximum) of the fitness.
-
     Note that you need to calculate the average (over the 10 runs) of the mean and maximum (over the population in each generation).
-
     Do one plot by enemy, thus, separately.
     """
 
@@ -64,33 +76,29 @@ def specialist_lineplots(df: pd.DataFrame):
     plt.show()
 
 
-def specialist_boxplots(df_res):
+def specialist_boxplots(df: pd.DataFrame):
     """
     Compare your algorithms by enemy, testing 5 times your final best solution for each of the 10 independent runs, and present the individual gain in box-plots.
     Note that you need to calculate the means of the 5 times for each solution of the algorithm for the enemy, and these means are the values that will be points in the box-plot. In summary, it is a total of 3 pairs of box-plots (so 6 boxes), being one pair per enemy.
     """
 
     # Get
-    df_avg = df_res.groupby(['ea_instance', 'enemy', 'individual'], as_index=False).mean()
-    df_avg.boxplot('gain', by=['enemy', 'ea_instance'])
+    df.boxplot('gain', by=['enemy', 'ea_instance'])
     degrees = 70
     plt.xticks(rotation=degrees)
 
-    print(df_avg)
+    print(df)
     plt.xticks([1, 2, 3, 4, 5, 6], [str(1) + ' ea_1', str(1) + ' ea_2', str(3) + ' ea_1',
                                     str(3) + ' ea_2', str(5) + ' ea_1', str(5) + ' ea_2'])
 
 
-    # Perform statistical tests
-    stat_test_t(df_avg)
-    plt.savefig('boxplots.jpg', dpi=300)
     plt.show()
 
 
-def stat_test_t(df_avg):
-    enemy_1 = df_avg.loc[df_avg['enemy'] == 1]
-    enemy_3 = df_avg.loc[df_avg['enemy'] == 3]
-    enemy_5 = df_avg.loc[df_avg['enemy'] == 5]
+def stat_test_t(df):
+    enemy_1 = df.loc[df['enemy'] == 1]
+    enemy_3 = df.loc[df['enemy'] == 3]
+    enemy_5 = df.loc[df['enemy'] == 5]
     enemies = [enemy_1, enemy_3, enemy_5]
     counter = 1
 
@@ -115,15 +123,20 @@ def stat_test_t(df_avg):
 
 
 if __name__ == "__main__":
-    # online_results = pd.read_csv(
-    #     'experiment_run/09-15-20_15_36_all_results.csv')
+    online_results = pd.read_csv(
+        'results/09-16-19_53_05_online_results.csv')
+    offline_results = pd.read_csv('dummy_offline_results.csv')
 
     # Format the data and calculate statistics
-    # df_summary = format_online_results(online_results)
+    online_summary = format_online_results(online_results)
+    offline_summary = format_offline_results(offline_results)
 
     # Specialist lineplots
-    # specialist_lineplots(df_summary)
+    specialist_lineplots(online_summary)
 
-    # boxplots
-    df_results = pd.read_csv('dummy_offline_results.csv')
-    specialist_boxplots(df_results)
+    # Specialist boxplots
+    specialist_boxplots(offline_summary)
+
+    # Significance tests
+    stat_test_t(offline_summary)
+
